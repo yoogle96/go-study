6e34ed345aca5e813e6af565695019d5ff5c4a01
dmltjs851 <dmltjs851@gmail.com>
2020-11-08 23:07:26 +0900

    Complete Return and handle an error

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("yoogle")
+	log.SetPrefix("grettins: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
