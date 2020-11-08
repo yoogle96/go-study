d886953881b998b347aa6d2976c6c37d2a6dd4fa
dmltjs851 <dmltjs851@gmail.com>
2020-11-08 23:39:41 +0900

    Complete Return a random greeting

--- hello/hello.go
+++ hello/hello.go
@@ -11,7 +11,7 @@ func main() {
 	log.SetPrefix("grettins: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("yoogle")
 	if err != nil {
 		log.Fatal(err)
 	}
